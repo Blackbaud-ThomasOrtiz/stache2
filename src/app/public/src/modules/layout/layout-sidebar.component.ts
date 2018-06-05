ff2ab0255a8f1d09d9b6659a4a3c0fa62778b85b
bdjones2410 <brandon.jones@blackbaud.com>
2018-06-05 15:45:26 -0400

    moved wrapper to new component

--- src/app/public/src/modules/layout/layout-sidebar.component.ts
+++ src/app/public/src/modules/layout/layout-sidebar.component.ts
@@ -1,12 +1,8 @@
-import { Component, Input, Renderer2 } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Component, Input } from '@angular/core';
 
 import { StacheLayout } from './layout';
-import { InputConverter, StacheOmnibarAdapterService, StacheWindowRef } from '../shared';
+import { InputConverter } from '../shared';
 import { StacheNavLink } from '../nav';
-import { Subject } from 'rxjs';
-
-const WINDOW_SIZE_MID = 992;
 
 @Component({
   selector: 'stache-layout-sidebar',
@@ -41,52 +37,4 @@ export class StacheLayoutSidebarComponent implements StacheLayout {
   @Input()
   @InputConverter()
   public showTableOfContents: boolean;
-
-  public sidebarClosed: boolean = false;
-
-  private ngUnsubscribe: Subject<any> = new Subject();
-
-  constructor(
-    private renderer: Renderer2,
-    private omnibarService: StacheOmnibarAdapterService,
-    private windowRef: StacheWindowRef
-  ) {
-    this.windowRef.onResize$
-      .takeUntil(this.ngUnsubscribe)
-      .subscribe(() => {
-        this.checkWindowWidth();
-      });
-  }
-
-  public ngOnInit() {
-    let omnibarHeight = this.omnibarService.getHeight();
-    let sidebarWrapperEl = this.windowRef.nativeWindow.document.querySelector('#stache-sidebar-wrapper');
-    this.renderer.setStyle(sidebarWrapperEl, 'top', `${omnibarHeight}px`);
-    this.checkWindowWidth();
-  }
-
-  public closeSidebar() {
-    this.sidebarClosed = true;
-  }
-
-  public openSidebar() {
-    this.sidebarClosed = false;
-  }
-
-  private checkWindowWidth() {
-    let windowWidth = this.windowRef.nativeWindow.innerWidth;
-
-    if (windowWidth < WINDOW_SIZE_MID) {
-      this.sidebarClosed = true;
-    }
-
-    if (windowWidth > WINDOW_SIZE_MID) {
-      this.sidebarClosed = false;
-    }
-  }
-
-  public ngOnDestroy() {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
-  }
 }
---
+++ src/app/public/src/modules/sidebar/sidebar-wrapper.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnInit, Renderer2, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { StacheNavLink } from '../nav';
+import { StacheOmnibarAdapterService, StacheWindowRef } from '../shared';
+
+const WINDOW_SIZE_MID = 992;
+
+@Component({
+  selector: 'stache-sidebar-wrapper',
+  templateUrl: './sidebar-wrapper.component.html',
+  styleUrls: ['./sidebar-wrapper.component.scss']
+})
+export class StacheSidebarWrapperComponent implements OnInit, OnDestroy {
+  @Input()
+  public sidebarRoutes: StacheNavLink[];
+
+  public sidebarClosed: boolean = false;
+
+  private ngUnsubscribe: Subject<any> = new Subject();
+
+  constructor(
+    private renderer: Renderer2,
+    private omnibarService: StacheOmnibarAdapterService,
+    private windowRef: StacheWindowRef
+  ) {
+    this.windowRef.onResize$
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(() => {
+        this.checkWindowWidth();
+      });
+  }
+
+  public ngOnInit(): void {
+    this.setTopAffix();
+    this.checkWindowWidth();
+  }
+
+  public setTopAffix(): void {
+    let omnibarHeight = this.omnibarService.getHeight();
+    let sidebarWrapperEl = this.windowRef.nativeWindow.document.querySelector('#stache-sidebar-wrapper');
+    this.renderer.setStyle(sidebarWrapperEl, 'top', `${omnibarHeight}px`);
+  }
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  public closeSidebar(): void {
+    this.sidebarClosed = true;
+  }
+
+  public openSidebar(): void {
+    this.sidebarClosed = false;
+  }
+
+  private checkWindowWidth(): void {
+    let windowWidth = this.windowRef.nativeWindow.innerWidth;
+
+    if (windowWidth < WINDOW_SIZE_MID) {
+      this.sidebarClosed = true;
+    }
+
+    if (windowWidth > WINDOW_SIZE_MID) {
+      this.sidebarClosed = false;
+    }
+  }
+}
